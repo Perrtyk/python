f17990bf21f0fdea0dfbc1ffa4e8d24887894470
Patryk Kostek <kostek.patryk@gmail.com>
2023-03-04 20:33:53 -0800

    UI Update and Optimize
    - Upgrade from pack to grid for better scalability.
    - Improved UI
    - hostname gathering now compatible with most devices, struggling with linux

--- Testing/IP Scanner.py
+++ Testing/IP Scanner.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, scrolledtext
+from tkinter.font import Font
 import threading
 import ipaddress
 import subprocess
@@ -22,8 +23,8 @@ def print_debug(msg):
     global currentTime
     currentTime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     print(msg)
-    term.insert(tk.END, msg + '\n')
-    term.see(tk.END)
+    output_console.insert(tk.END, msg + '\n')
+    output_console.see(tk.END)
 
 
 def get_ping(ip_address):
@@ -32,7 +33,7 @@ def get_ping(ip_address):
         return ping_time
     print_debug(f'[{currentTime}] IP Scan ({ip_address}): Running PING process . . .\n')
     ping_response = subprocess.Popen(['ping', '-n', '3', '-w', '350', ip_address],
-                                         stdout=subprocess.PIPE).communicate()[0]
+                    stdout=subprocess.PIPE).communicate()[0]
     if b'Reply from' in ping_response:
         ping_time = str(ping_response).split("Average =")[1].split("ms")[0] + ' ms'
         print_debug(f'[{currentTime}] IP Scan ({ip_address}): Received ping response of{ping_time}.\n')
@@ -104,15 +105,18 @@ def get_mac_address(ip_address):
 def get_host_name(ip_address):
     print_debug(f'[{currentTime}] IP Scan ({ip_address}): Gathering hostname . . .\n')
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(0.2)
-            s.connect((ip_address, 80))
-            hostname = socket.gethostbyaddr(ip_address)[0]
+        hostname = socket.gethostbyaddr(ip_address)[0]
         print_debug(f'[{currentTime}] IP Scan ({ip_address}): Hostname is {hostname}\n')
-    except (socket.gaierror, socket.timeout, ConnectionRefusedError):
-        hostname = '.N/A'
-        print_debug(f'[{currentTime}] IP Scan ({ip_address}): Hostname lookup failed\n')
-    return hostname
+        return hostname
+    except socket.herror as e:
+        print_debug(f'[{currentTime}] IP Scan ({ip_address}): Error resolving hostname: {e}\n')
+        hostname = 'N/A'
+        return hostname
+    except Exception as e:
+        print_debug(f'[{currentTime}] IP Scan ({ip_address}): Error: {e}\n')
+        hostname = 'N/A'
+        return hostname
+
 
 # Define the function to scan a range of IP addresses and display the results in a table
 def scan_ip_range(start_ip, end_ip, progress_var):
@@ -149,20 +153,22 @@ def scan_ip_range(start_ip, end_ip, progress_var):
             print_debug(f'[{currentTime}] IP Scan ({ip_address}): Skipping, host is not available.\n')
             progress_var.set(progress_var.get() + 1)
             continue
+        else:
+            mac_address = get_mac_address(ip_address)
 
-        mac_address = get_mac_address(ip_address)
+            ping_time = get_ping(ip_address)
 
-        ping_time = get_ping(ip_address)
+            hostname = get_host_name(ip_address)
 
-        hostname = get_host_name(ip_address)
+            progress_var.set(progress_var.get() + 1)
 
-        progress_var.set(progress_var.get() + 1)
+            print_debug(f"[{currentTime}] Processed IP Address: {ip_address} , Processed Ping: {ping_time}"
+                        f" , Processed Hostname: {hostname}\n"
+                        f"[{currentTime}] Processed MAC: {mac_address} ,"
+                        f"Processed Connectivity: {available}\n")
 
-        print_debug(f"[{currentTime}] Processed IP Address: {ip_address} , Processed Ping: {ping_time} , Processed Hostname: {hostname}\n"
-                    f"[{currentTime}] Processed MAC: {mac_address} , Processed Connectivity: {available}\n")
-
-        # Add a row to the treeview with the IP address, hostname, MAC address, ping time, and availability
-        results_treeview.insert('', 'end', values=(ip_address, hostname, mac_address, ping_time, available))
+            # Add a row to the treeview with the IP address, hostname, MAC address, ping time, and availability
+            results_treeview.insert('', 'end', values=(ip_address, hostname, mac_address, ping_time, available))
 
     else:
         print_debug(f'[{currentTime}] Scan completed.')
@@ -182,73 +188,93 @@ def disable_scan_button():
     scan_in_progress = True
     scan_button.config(state="disabled", text="Scanning...")
 
+
 def enable_scan_button():
     global scan_in_progress, scan_button
     scan_in_progress = False
     scan_button.config(state="normal", text="Scan")
 
+
 # Create the main window
 root = tk.Tk()
 root.title('IP Scanner')
+root.columnconfigure(0, weight=1)
 
 # Create the input frame
-input_frame = ttk.Frame(root, padding=(10, 10, 10, 0))
-input_frame.pack(fill='x')
+input_frame = ttk.Frame(root, padding=(10, 10, 10, 30))
+input_frame.grid(column=0, row=0, sticky='nsew')
+input_frame.columnconfigure(0, weight=1, minsize=1)
+input_frame.rowconfigure(0, weight=1, minsize=1)
+
+# Configure the input frame rows and columns
+input_frame.rowconfigure(0, weight=0)
+input_frame.rowconfigure(1, weight=1, pad=5)  # decreased pady value
+input_frame.rowconfigure(2, weight=1, pad=5)  # decreased pady value
+input_frame.rowconfigure(3, weight=1, pad=5)  # decreased pady value
+input_frame.columnconfigure(0, weight=1)
+input_frame.columnconfigure(1, weight=1)
+input_frame.columnconfigure(2, weight=1)
+input_frame.columnconfigure(3, weight=1)
 
 # Create the start IP address label and entry
 start_ip_label = ttk.Label(input_frame, text='Start IP Address:')
-start_ip_label.pack(side='left', padx=(0, 10))
+start_ip_label.grid(column=0, row=1, padx=(0, 10), sticky='we')
 start_ip_entry = ttk.Entry(input_frame)
 start_ip_entry.insert(0, '192.168.1.1')
-start_ip_entry.pack(side='left', fill='x', expand=True)
+start_ip_entry.grid(column=1, row=1, padx=(0, 20), sticky='we')
 
 # Create the end IP address label and entry
 end_ip_label = ttk.Label(input_frame, text='End IP Address:')
-end_ip_label.pack(side='left', padx=(10, 10))
+end_ip_label.grid(column=2, row=1, padx=(10, 0), sticky='we')
 end_ip_entry = ttk.Entry(input_frame)
 end_ip_entry.insert(0, '192.168.1.255')
-end_ip_entry.pack(side='left', fill='x', expand=True)
+end_ip_entry.grid(column=3, row=1, sticky='we')
 
-# Create the Scan button
-scan_button = ttk.Button(root, text='Scan', command=lambda: threading.Thread(target=scan_ip_range, args=(start_ip_entry.get(), end_ip_entry.get(), progress_var)).start())
-scan_button.pack(pady=(10, 0))
+# Create a custom style with a larger font size
+button_style = ttk.Style()
+button_style.configure('button_style.TButton', font=('Arial', 10), padding=1, sticky='nsew', pady=(0, 0), width=5,)
+
+# Create the Scan button with a fixed width and the custom style
+scan_button = ttk.Button(input_frame, text='Scan', style='button_style.TButton', command=lambda: threading.Thread(target=scan_ip_range, args=(start_ip_entry.get(), end_ip_entry.get(), progress_var)).start())
+scan_button.grid(column=0, row=2, pady=(5, 0), sticky='we')
 
 # Create the Stop Scan button
 stop_scan_button = ttk.Button(input_frame, text='Stop Scan', command=stop_scan_func)
-stop_scan_button.pack(side='left', pady=(10, 0))
+stop_scan_button.grid(column=3, row=2, pady=(5, 0), sticky='we')  # decreased pady value
 
 # Create the results frame
 results_frame = ttk.Frame(root, padding=(10, 0))
-results_frame.pack(fill='both', expand=True)
+results_frame.grid(column=0, row=1, sticky='nsew')
+root.rowconfigure(1, weight=1)
+results_frame.columnconfigure(0, weight=1)
 
 # Create the results treeview
-results_columns = ('IP Address', 'Hostname', 'MAC Address', 'Ping Time', 'Available')
+results_columns = ('IP Address', 'Hostname', 'MAC Address', 'Ping', 'Connectivity')
 results_treeview = ttk.Treeview(results_frame, columns=results_columns, show='headings')
+results_treeview.grid(column=0, row=0, sticky='nsew')
+
+# Configure the results treeview columns
 for column in results_columns:
     results_treeview.heading(column, text=column)
+    results_treeview.column(column, anchor='center')
     results_treeview.column(column, width=150)
 
-# Create the scrollbar for the results treeview
+# Create a scrollbar for the results treeview
 results_scrollbar = ttk.Scrollbar(results_frame, orient='vertical', command=results_treeview.yview)
-
-# Attach the scrollbar to the treeview
-results_treeview.config(yscrollcommand=results_scrollbar.set)
-results_scrollbar.pack(side='right', fill='y')
-results_treeview.pack(fill='both', expand=True)
+results_scrollbar.grid(column=1, row=0, sticky='ns')
+results_treeview.configure(yscrollcommand=results_scrollbar.set)
 
 # Create the progress bar
-progress_var = tk.IntVar()
-progress_var.set(0)
-progress_bar = ttk.Progressbar(root, variable=progress_var, maximum=100)
-progress_bar.pack(fill=tk.X, padx=10, pady=10)
+progress_var = tk.DoubleVar()
+progress_bar = ttk.Progressbar(input_frame, variable=progress_var, maximum=100, value=0)
+progress_bar.grid(column=0, row=3, columnspan=4, sticky='we')
+percentage_label = ttk.Label(root, text='', anchor=tk.CENTER, font='TkDefaultFont 10 bold')
+percentage_label.grid(row=6, column=0, columnspan=4, pady=5)
 
-percentage_label = ttk.Label(root, text='0%')
-percentage_label.pack(pady=10)
+# Create the output console
+console_font = Font(family='Consolas', size=8, weight='bold')
+output_console = scrolledtext.ScrolledText(root, state='normal', height=11, font=console_font)
+output_console.grid(column=0, row=2, sticky='nsew')
 
-# create a scrolled text widget
-term = scrolledtext.ScrolledText(root, height=12)
-term.pack(side=tk.BOTTOM, fill=tk.X)
-
+# Start the main loop
 root.mainloop()
-
-#get_host_name('192.168.1.1')
